8212d35e20012adbadd4edbb539888ffa7b7eff8
Yuta Fujii <y.f.mar2@gmail.com>
2018-12-13 18:40:24 +0900

    Make input and output components and add functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
 
 class App extends Component {
+  state = {
+    users: [
+      {name: "yuta"},
+      {name: "Mike"}
+    ]
+  }
+
+  changeNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This is Yuta Fujii <code>src/App.js</code>.
-          </p>
-          <Person name="Yuta" age="25" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nulla quibusdam, obcaecati tempore tenetur hic et, facere consectetur fugit aliquam provident velit asperiores atque, veritatis aperiam ipsum voluptatem eos commodi.
-          </a>
-        </header>
+        <UserInput change={this.changeNameHandler} />
+        <UserOutput name={this.state.users[0].name}/>
+        <UserOutput name={this.state.users[1].name}/>
       </div>
     );
   }
---
+++ my-app/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+  return (
+    <div className="hello">
+      <p>{props.name}</p>
+      <p>Le Wagon187</p>
+    </div>
+  );
+}
+
+export default userOutput;
